6e3bcb7b0da171f44f509095c3a52a72e7f9e277
widelec <5738944+widelec-BB@users.noreply.github.com>
2018-05-03 19:29:41 +0200

    Increase stack size.

--- Aquaria/Main.cpp
+++ Aquaria/Main.cpp
@@ -30,6 +30,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 	#include <shellapi.h>
 #endif
 
+#ifdef __MORPHOS__
+int __stack = 1048576;
+#endif
 
 static void MakeRan(void)
 {
